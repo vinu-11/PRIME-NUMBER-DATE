2c076c43b982092d00e8dfb25b5ffd1a5acd9b08
Vinay kumar <hundekarvinay@gmail.com>
2023-03-03 20:20:27 +0530

    Added some test cases

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
 var readLineSync = require('readline-sync');
 var userDate = readLineSync.question('Please enter ur date in DD format: ');
-var flag = 0;
+var userMonth = readLineSync.question('Please enter ur month in MM format: ');
 
-if (userDate == 0 || userDate == 1){
-  flag = 1;
+if ( userMonth == 02 && userDate < 29){
+  primeNumber(userDate);
+} else if ( userMonth < 31 && userMonth == 04 || userMonth == 06 || userMonth == 08 || userMonth == 09 || userMonth ==11 ){
+  primeNumber(userDate);
+}else if (userMonth < 32 && userMonth == 01 || userMonth == 03 || userMonth == 05 || userMonth == 07 || userMonth ==10 || userMonth ==12){
+  primeNumber(userDate);
+}else{
+  console.log("Please enter valid date and month");
 }
-for(var i = 2; i <= userDate / 2; i++){
-  if ( userDate%i == 0){
+
+function primeNumber(userDate){
+  var flag = 0;
+
+  if (userDate == 0 || userDate == 1){
+    flag = 1;
+  }
+  for(var i = 2; i <= userDate / 2; i++){
+    if ( userDate%i == 0){
     flag = 1;
     break;
+    }
+  }
+
+  if(flag == 0){
+    console.log("Entered date is prime number ");
+  }else{
+    console.log("Entered date is not a prime number ");
   }
 }
 
-if(flag == 0){
-  console.log("Entered date is prime number ");
-}else{
-  console.log("Entered date is not a prime number ");
-}
